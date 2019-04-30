47b169c5eee6e5412d91f15d682b64fa75d38bf0
Greta Yorsh <gyorsh@janestreet.com>
2019-04-30 16:01:16 +0100

    Add copyright

--- asmcomp/reoptimize.ml
+++ asmcomp/reoptimize.ml
@@ -1,3 +1,21 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                     Greta Yorsh, Jane Street Europe                    *)
+(*                                based on                                *)
+(*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           *)
+(*                                                                        *)
+(*   Copyright 2019 Jane Street Group LLC                                 *)
+(*   Copyright 1996 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
 [@@@ocaml.warning "+a-4-30-40-41-42"]
 (* Annotate linear IR with debug information that ties generated assembly and
    binaries back to linear IR for profile-guided optimizations.
