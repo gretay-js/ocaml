af23ebb536fc681d8eb0eb2ccbd721b5a60ecc18
Greta Yorsh <gyorsh@janestreet.com>
2019-04-30 16:01:16 +0100

    Add copyright

--- asmcomp/reoptimize.mli
+++ asmcomp/reoptimize.mli
@@ -1,3 +1,21 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                     Greta Yorsh, Jane Street Europe                    *)
+(*                                based on                                *)
+(*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           *)
+(*                                                                        *)
+(*   Copyright 2019 Jane Street Group LLC                                 *)
+(*   Copyright 1996 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
 (* Machine specific optimizations. *)
 
 (** Registers function [f] as the transformer to be applied
