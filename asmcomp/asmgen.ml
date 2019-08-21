dc91cf53e4b960dc6e7ba0041dd0035d3acb29ac
Greta Yorsh <gyorsh@janestreet.com>
2019-08-21 13:06:52 +0100

    rev

--- asmcomp/asmgen.ml
+++ asmcomp/asmgen.ml
@@ -59,6 +59,7 @@ let save_linear f =
 let write_linear output_prefix =
   if !should_save_after_linearize then
     let filename = output_prefix ^ Clflags.Compiler_ir.(extension Linear) in
+    linear_unit_info.items <- List.rev linear_unit_info.items;
     Linear_format.save filename linear_unit_info
 
 let dump_if ppf flag message phrase =
