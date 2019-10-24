78c84adc6de5d71e40f330629330f4e4e383a192
Greta Yorsh <gyorsh@janestreet.com>
2019-10-24 20:08:01 +0100

    Provide reason of test failure

--- testsuite/tests/tool-ocamlopt-stop-after/stop_after_scheduling.sh
+++ testsuite/tests/tool-ocamlopt-stop-after/stop_after_scheduling.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
+set -e
+
 asm=${test_build_directory}/stop_after_scheduling.${asmext}
 obj=${test_build_directory}/stop_after_scheduling.${objext}
 cmx=${test_build_directory}/stop_after_scheduling.cmx
 
 # Check that cmx is generated but asm and obj are not
-if [ -e "$asm" ]; then
-    test_result=${TEST_FAIL};
-else if [ -e "$obj" ]; then
-         test_result=${TEST_FAIL};
-     else if [ -e "$cmx" ]; then
-              test_result=${TEST_PASS};
+if [ -e "$asm" ] ; then
+    echo "unexpected $asm found" > ${ocamltest_response}
+    test_result=${TEST_FAIL}
+else if [ -e "$obj" ] ; then
+         echo "unexpected $obj found" > ${ocamltest_response}
+         test_result=${TEST_FAIL}
+     else if [ -e "$cmx" ] ; then
+              test_result=${TEST_PASS}
           else
-              test_result=${TEST_FAIL};
+              echo "not found expected $cmx" > ${ocamltest_response}
+              test_result=${TEST_FAIL}
           fi
      fi
 fi
+exit ${test_result}
