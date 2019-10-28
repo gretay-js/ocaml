a160eb6a0018bf9eded7eba6c40d4a5ed316329b
Greta Yorsh <gyorsh@janestreet.com>
2019-10-28 10:26:12 +0000

    Address review comments

--- utils/clflags.ml
+++ utils/clflags.ml
@@ -450,14 +450,13 @@ module Compiler_pass = struct
 
   let enabled is_native t = not (is_native_only t) || is_native
 
-  let pass_names filter is_native =
+  let pass_names is_native =
     passes
     |> List.filter (enabled is_native)
-    |> List.filter filter
     |> List.map to_string
 
   let stop_after_pass_names ~native =
-    pass_names (fun _ -> true) native
+    pass_names native
 end
 
 let stop_after = ref None (* -stop-after *)
