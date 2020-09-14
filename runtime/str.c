a86aa489b52fc20b60c36c8206aa301a0cf15b38
Greta Yorsh <gyorsh@janestreet.com>
2020-09-14 12:36:22 +0100

    Reimplement caml_string_equal using memcmp

--- runtime/str.c
+++ runtime/str.c
@@ -271,15 +271,15 @@ CAMLprim value caml_bytes_set64(value str, value index, value newval)
 CAMLprim value caml_string_equal(value s1, value s2)
 {
   mlsize_t sz1, sz2;
-  value * p1, * p2;
 
   if (s1 == s2) return Val_true;
   sz1 = Wosize_val(s1);
   sz2 = Wosize_val(s2);
   if (sz1 != sz2) return Val_false;
-  for(p1 = Op_val(s1), p2 = Op_val(s2); sz1 > 0; sz1--, p1++, p2++)
-    if (*p1 != *p2) return Val_false;
-  return Val_true;
+  if (!memcmp(String_val(s1), String_val(s2), Bsize_wsize(sz1)))
+    return Val_true;
+  else
+    return Val_false;
 }
 
 CAMLprim value caml_bytes_equal(value s1, value s2)
