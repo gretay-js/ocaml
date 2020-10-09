e24579206e3797a890da6cb0394e8b6da7676cb9
Greta Yorsh <gyorsh@janestreet.com>
2020-10-09 16:16:18 +0100

    fix perfmon test

--- testsuite/tests/intrinsics/perfmon.ml
+++ testsuite/tests/intrinsics/perfmon.ml
@@ -1,41 +1,39 @@
 (* TEST
- * bytecode
- * native
+ * arch_amd64
+ ** bytecode
+    reference = "${test_source_directory}/perfmon.byte.reference"
+ ** native
+    reference = "${test_source_directory}/perfmon.opt.reference"
+    compare_programs = "false"
 *)
-
-(* how to enable it only for amd64? *)
 
 external perfmon : string -> int64 -> int64  = "%perfmon"
 
-let rdtsc () = perfmon "rdtsc" 0n
-let rmpmc c = perfmon "rdpmc"
+let rdtsc () = perfmon "rdtsc" 0L
+let rdpmc = perfmon "rdpmc"
 
-let[@inline never] opaque x = Sys.opaque_identity x
-
-let work () =
-  let min = 0, max = 100 in
-  let e = ref 0L in
+let [@inline never] work () =
+  let min = 0 in
+  let max = 100 in
+  let e = ref 0 in
   for i = min to max do
-    e :=  !e + Int64.to_int i
+    e :=  !e + i
   done;
-  opaque !e
+  Sys.opaque_identity !e
+  |> ignore
 
 let test_rdtsc () =
-  let before = rdtsc () in begin
-    work ();
-    let after = rdtsc () in
-    (* assert before <> after *)
-  end;
-  ()
+  let before = rdtsc () in
+  work ();
+  let after = rdtsc () in
+  Printf.printf "%b\n" (Int64.equal before after)
 
-let test_rdtsc () =
+let test_rdpmc () =
   let c = 0L in
-  let before = rdpmc c in begin
-    work ();
-    let after = rdpmc c in
-    (* assert before <> after *)
-  end;
-  ()
+  let before = rdpmc c in
+  work ();
+  let after = rdpmc c in
+  Printf.printf "%b\n" (Int64.equal before after)
 
 let test =
   test_rdtsc ();
